0f1a17dda6b4390befc36dbe75a2a5c984f9729d
mitschi11051994 <mitschi1994@hotmail.com>
2017-04-25 14:21:37 -0600

    ADD vista Home
    Añadió home_view

---
+++ application/views/home_view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home </title>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo base_url(); ?>home">HOME  My Pets Veterinary Center </a>
+
+    </div>
+    <div class="collapse navbar-collapse" id="navbar1">   
+      <ul class="nav navbar-nav navbar-right">  
+      	<li><a href="<?php echo base_url(); ?>login">Login</a></li>
+        <li><a href="<?php echo base_url(); ?>registration">Signup </a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div  class="container">
+  		<div  class="row">
+  			<div class="panel panel-success" style="width: 57em; height: 33em; margin-left: 13em; padding: 2em; margin-top: 3em;" >
+
+		  		<div  class="panel-heading">
+		  			<img class="profile-img" src="https://s-media-cache-ak0.pinimg.com/564x/04/a2/6a/04a26aec29172cc84951c6f27540f35b.jpg" style="width: 400px; height: 27em; margin-left: 11em; margin-top: 5px; padding: 2em; padding-bottom: -1em; border-radius: 20%;">
+		  		
+		  		</div>			
+  			</div>
+  		</div>
+  	</div>
+
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+</body>
+</html>
