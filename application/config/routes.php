1b8f01d52962f7dbd5c88ee6e60bec316eeebb91
mitschi11051994 <mitschi1994@hotmail.com>
2017-04-28 13:14:39 -0600

    Edit Route

--- application/config/routes.php
+++ application/config/routes.php
@@ -199,3 +199,11 @@ $route['insertPet_vacuna_enfermedad'] = "/Users/insertPet_vacuna_enfermedad";
 
 /* Controlador encargado de la ruta del pet_vacuna_enfermedad, el cual es para despues de haber ingresado correctamente el login, dentro de New Case File y respectivamente el pet_vacuna_enfermedad, este es el mantenimiento de Edición*/
 $route['users/editPet_vacuna_enfermedad/(:any)'] = '/Users/edit_pet_vacuna_enfermedad/$1';
+
+/*-------------------        Consultas   ---------------------------------*/
+
+/* Controlador encargado de la ruta de consult_disease*/
+$route['consult_disease'] = "/Users/consult_disease";
+
+/* Controlador encargado de la ruta de consult_disease*/
+$route['consult_vaccine'] = "/Users/consult_vaccine";
