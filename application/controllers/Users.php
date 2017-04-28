66ba5cd60e5334a59fb268b26592551f05c243d9
mitschi11051994 <mitschi1994@hotmail.com>
2017-04-28 10:36:05 -0600

    Edit Controller and View Insert disease

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -848,9 +848,8 @@ class users extends CI_Controller {
 
             if($this->form_validation->run() == true){
                 $consulta= $this->user->obtener_por_id_vaccine_disease($this->input->post('cod_enfermedad'));
-
-                $consulta_owner= $this->user->obtener_por_id_vaccine_disease_exist($this->input->post('cod_mascota'));
-                if($consulta == false $ $consulta_owner == false){
+                $consulta_vaccine_disease= $this->user->obtener_por_id_vaccine_disease_exist($this->input->post('cod_mascota'));
+                if($consulta == false & $consulta_vaccine_disease == false){
                     $insert = $this->user->guardarVaccine_disease($userData);
                     if($insert){
                         $this->session->set_userdata('success_msg', 'Your registration was successfully. Please login to your account.');
