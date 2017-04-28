32c4888c09830e8734ceee50fbf8415154bf9305
mitschi11051994 <mitschi1994@hotmail.com>
2017-04-28 12:21:12 -0600

    Add new View

---
+++ application/views/consult/consult_disease.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pet </title>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo base_url(); ?>home">HOME </a>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>">My Pets Veterinary Center</a>
+
+    </div>
+    <div class="collapse navbar-collapse" id="navbar1">   
+      <ul class="nav navbar-nav navbar-right">      
+        <li><a href="<?php echo base_url(); ?>users/logout" class="logoutBtn">Logout</a></li>
+        <li><a>Welcome <?php echo $user['name']; ?>!<a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div  class="container">
+
+  <div  class="row">
+  <div class="panel panel-success" style="width: 90em; height: 50em; margin-left: -3em; padding: 2em; margin-top: -1em;" >
+
+  <div  class="panel-heading" style="width: 86em; height: 8em;">
+
+      <img class="profile-img" src="https://s-media-cache-ak0.pinimg.com/564x/04/a2/6a/04a26aec29172cc84951c6f27540f35b.jpg" style="width: 95px; height: 6em; margin-left: 2em; margin-top: 5px; padding-top: -1em; padding-bottom: -1em; border-radius: 20%; border-radius: 20%;">
+
+      <div class="panel-body" style="width: 50em; margin-left: 0em; margin-top: 4em;  padding: 5px 10px;"> 
+      
+        <div>
+        <h1> PET FILE </h1>
+                <p> <a class="btn btn-success" href="<?php echo base_url() ?>new_case_file"> BACK</a> 
+                 <a class="btn btn-success" href="<?php echo base_url() ?> pet_register"> Create New Pet  </a> 
+                 <a class="btn btn-success" href="<?php echo base_url() ?>pet_vacuna_enfermedad"> Create New Vaccine for Pet  </a> 
+                 <a class="btn btn-success" href="<?php echo base_url() ?>"> Consult Disease  </a>
+                 <a class="btn btn-success" href="<?php echo base_url() ?>"> Consult Vaccine  </a> </p>
+                 
+
+                    <?php if (count($pet)): ?>
+                        <table style="width: 72em;" class="table tableborder">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Código Mascota</th>
+                                    <th scope="col">Identidad Dueño</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Fecha Nacimiento</th>
+                                    <th scope="col">Raza</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                foreach ($pet as $item){
+                                ?>
+                                  <tr>
+                                    <td style="width: 35%"> <?php echo $item->cod_mascota ?></td>
+                                    <td style="width: 35%"> <?php echo $item->id?> </td>
+                                    <td style="width: 35%"> <?php echo $item->nombre ?></td>
+                                    <td style="width: 35%"> <?php echo $item->fecha_nacimiento?></td>
+                                    <td style="width: 35%"> <?php echo $item->cod_raza?></td>
+
+                                    <td ><a class="btn btn-info" href="<?php echo base_url() ?>users/editPet/<?php echo $item->cod_mascota ?>">Edit</a></td>
+
+                                    <td><a class="btn btn-danger eliminar_pet" href="<?php echo base_url() ?>Users/eliminarPet/<?php echo $item->cod_mascota ?>"> Eliminar </a> </td>
+                                  </tr>
+                                <?php
+                                }
+                                ?>                                
+                            </tbody>
+                        </table>
+                    <?php else: ?>
+                        <p> No hay owner </p>
+                    <?php endif; ?>
+                    <script type="text/javascript">
+                        $(".eliminar_pet").each(function() {
+                            var href = $(this).attr('href');
+                            $(this).attr('href', 'javascript:void(0)');
+                            $(this).click(function() {
+                                if (confirm("¿Seguro desea eliminar este dueo?")) {
+                                    location.href = href;
+                                }
+                            });
+                        });
+                    </script>
+         </div>
+       </div>
+     </div>
+  </div>
+</div>
